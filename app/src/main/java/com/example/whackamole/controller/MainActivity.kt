eb2ab68573e86add52546751343c783dda634d4f
Jesutofunmi Obimakinde <joo@iastate.edu>
2025-10-02 03:20:46 -0500

    addition of mainviewmodel

--- app/src/main/java/com/example/whackamole/controller/MainActivity.kt
+++ app/src/main/java/com/example/whackamole/controller/MainActivity.kt
@@ -6,6 +6,8 @@ import com.example.whackamole.GameController
 import com.example.whackamole.GameModel
 import com.example.whackamole.GameView
 import com.example.whackamole.R
+import com.example.whackamole.repository.real.SharedPrefGameRepository
+import com.example.whackamole.viewmodel.MainViewModel
 
 /**
  * Main activity that initializes and manages the Whack-a-Mole game.
@@ -17,6 +19,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var model: GameModel
     private lateinit var view: GameView
     private lateinit var controller: GameController
+
+    // Sample Initialization of MainViewModel
+    private val mainViewModel: MainViewModel by lazy {
+        val prefs = getSharedPreferences("WhackAMolePrefs", MODE_PRIVATE)
+        val repository = SharedPrefGameRepository(prefs)
+        MainViewModel(repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
